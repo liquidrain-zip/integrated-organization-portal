6e2224a9d1969890e0b3cc9d968d0c69cff42415
Shabir Mohd Bashir <m.shabirmb@gmail.com>
2024-05-11 22:30:28 +0800

    init events

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+import { EventsService } from '../../service/events.service';
+
+interface UpcomingEvent {
+  title: string;
+  date: Date;
+  type: string;
+  attendance: number;
+}
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrl: './events.component.css'
+  styleUrl: './events.component.css',
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit {
+  upcomingEvents$: Observable<UpcomingEvent[]>;
+  selectedEvent: UpcomingEvent;
 
+  constructor(private eventsService: EventsService) {}
+
+  ngOnInit() {
+    this.upcomingEvents$ = this.eventsService
+      .getUpcomingEvents()
+      .pipe(catchError((error) => of([])));
+  }
+
+  selectEvent(event: UpcomingEvent) {
+    this.selectedEvent = event;
+  }
 }
